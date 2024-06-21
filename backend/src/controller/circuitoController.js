db000f74504bf44c8ed07d8526f3961284898498
henriqtorresl <henriquetlandin@gmail.com>
2024-06-21 16:29:06 -0300

    logica para receber o arquivo csv finalizada

--- backend/src/controller/circuitoController.js
+++ backend/src/controller/circuitoController.js
@@ -1,4 +1,4 @@
-const service = require('../service/service')
+const service = require('../service/circuitoService')
 
 const getAll = async (req, res) => {
     /*
@@ -6,10 +6,33 @@ const getAll = async (req, res) => {
         #swagger.description = 'Dados sobre o circuito'
         #swagger.summary = 'Trás todas as informações sobre os circuitos realizados pelo carrinho.'
     */
-   service.getAll();
-   res.status(200).json({msg: 'ola'});
+    service.getAll();
+    res.status(200).json({ msg: 'ola' });
+}
+
+const post = async (req, res) => {
+    /*
+        #swagger.tags = ['Circuito']
+        #swagger.description = ''
+        #swagger.summary = ''
+        #swagger.consumes = ['multipart/form-data']
+        #swagger.parameters['file'] = {
+            in: 'formData',
+            type: 'file',
+            required: true,
+            description: 'Arquivo para upload'
+        }
+    */
+
+    if (req.file) {
+        const fileContent = req.file.buffer.toString('utf-8');
+        console.log(fileContent);
+    }
+
+    res.status(200).json({ msg: 'ola' });
 }
 
 module.exports = {
-    getAll
+    getAll,
+    post
 }
--- backend/src/routes/circuitoRoutes.js
+++ backend/src/routes/circuitoRoutes.js
@@ -1,8 +1,12 @@
 const Router = require('express');
-const circuitoController = require('../controller/circuitoController')
+const controller = require('../controller/circuitoController')
+const multer = require('multer');
+
+const upload = multer();
 
 const circuitoRouter = Router();
 
-circuitoRouter.get('/circuito', circuitoController.getAll);
+circuitoRouter.get('/circuito', controller.getAll);
+circuitoRouter.post('/circuito', upload.single('file'), controller.post)
 
 module.exports = circuitoRouter;
