e189a327e4b0c715a5d1f504677106f6ee0d95de
henriqtorresl <henriquetlandin@gmail.com>
2024-06-23 20:33:13 -0300

    corrigindo o script ddl

--- backend/docs/script_ddl.sql
+++ backend/docs/script_ddl.sql
@@ -1,32 +1,8 @@
-CREATE TABLE car (
-    car_id BIGINT PRIMARY KEY,
-    car_weight BIGINT,
-    car_length BIGINT
-);
-
-CREATE TABLE sensor (
-    sensor_id BIGINT PRIMARY KEY,
-    car_id BIGINT,
-    type LINESTRING,
-    foreign key (car_id) references car(car_id)
-);
-
-CREATE TABLE leitura (
-    leitura_id BIGINT PRIMARY KEY,
-    sensor_id BIGINT,
-    circuito_id BIGINT,
+CREATE TABLE circuito (
+    circuito_id INTEGER PRIMARY KEY AUTOINCREMENT,
     instant_speed BIGINT,
     instant_acceleration BIGINT,
     energy_consumed BIGINT,
     travelled_distance BIGINT,
     travel_time TIME
-);
-
-CREATE TABLE circuito (
-    circuito_id BIGINT PRIMARY KEY,
-    car_id BIGINT,
-    leitura_id BIGINT,
-    length_circuito BIGINT,
-    foreign key (car_id) references car(car_id),
-    foreign key (leitura_id) references leitura(leitura_id)
 );
