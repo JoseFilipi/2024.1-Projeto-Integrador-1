38b33a3915c1ec6552b70f98f15ec1483aff1846
henriqtorresl <henriquetlandin@gmail.com>
2024-06-23 21:42:07 -0300

    corrigindo erro de cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('./swagger/swagger_output.json');
 const circuitoRoutes = require('./routes/circuitoRoutes');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(circuitoRoutes);
 
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerFile));
